dbc231993750387d1eb5388f2b01524ba2cdac81
aditi1224 <43275149+aditi1224@users.noreply.github.com>
2018-11-28 10:48:22 +0530

    Update Splittingfile_prog.c

--- Splittingfile_prog.c
+++ Splittingfile_prog.c
@@ -1,62 +1,78 @@
 /* c [rogram to split a file into equal parts */
-#include <stdio.h>
+  #include <stdio.h>
   #include <string.h>
   #define MAX 256
 
-  int main() {
-        int ch;
+  int main()
+  {
+        int i = 0, line = 0;
         FILE *fp1, *fp2, *fp3;
-        char fname1[MAX], fname2[MAX], temp[] = {"temp.txt"};
+        char file1[MAX], file2[MAX], file3[MAX], str[MAX];
 
-        /* get the first input file name from the user */
-        printf("Enter your first file name:");
-        fgets(fname1, MAX, stdin);
-        fname1[strlen(fname1) - 1] = '\0';
+        /* get the name without file format(.txt) from user */
+        printf("Enter your input file name:");
+        scanf("%s", file1);
 
-        /* get the second input file name from the user */
-        printf("Enter your second file name:");
-        fgets(fname2, MAX, stdin);
-        fname2[strlen(fname2) - 1] = '\0';
+        /* form the output file names */
+        sprintf(file2, "%s_part_%d.txt", file1, 1);
+        sprintf(file3, "%s_part_%d.txt", file1, 2);
+        strcat(file1, ".txt");
 
-        /* open first input file in read mode */
-        fp1 = fopen(fname1, "r");
+        /* open the input file in read mode */
+        fp1 = fopen(file1, "r");
 
         /* error handling */
         if (!fp1) {
-                printf("Unable to open input file %s\n", fname1);
+                printf("Unable to open input file in read mode!!\n");
                 return 0;
         }
 
-        /* open second input file in read mode */
-        fp2 = fopen(fname2, "r");
+        /* open the output file in write mode */
+        fp2 = fopen(file2, "w");
 
         /* error handling */
         if (!fp2) {
-                printf("Unable to open input file %s\n", fname2);
+                printf("Unable to open output file in write mode\n");
                 fclose(fp1);
                 return 0;
         }
 
-        /*
-         * open a temporary file to write the contents
-         * of first and second input files
-         */
-        fp3 = fopen(temp, "w");
+        /* open the output file in write mode */
+        fp3 = fopen(file3, "w");
 
         /* error handling */
         if (!fp3) {
-                printf("Unable to open temporary file to write!!\n");
+                printf("Unable to open output file2 in write mode\n");
+                fclose(fp1);
+                fclose(fp2);
                 return 0;
         }
 
-        /* copy the contents of first file to temporary file */
-        while ((ch = fgetc(fp1)) != EOF) {
-                fputc(ch, fp3);
+        /* calculate the number of lines in input file */
+        while (!feof(fp1)) {
+                fgets(str, MAX, fp1);
+                if (!feof(fp1))
+                        line++;
         }
 
-        /* copy the contents of second file to temporary file */
-        while ((ch = fgetc(fp2)) != EOF) {
-                fputc(ch, fp3);
+        /* moving the file pointer to the start of file */
+        rewind(fp1);
+
+        /* writing first half of data to first output file */
+        while (i < line/2) {
+                fgets(str, MAX, fp1);
+                if (!feof(fp1)) {
+                        fputs(str, fp2);
+                }
+                i++;
+        }
+
+        /* writing second half to second output file */
+        while (!feof(fp1)) {
+                fgets(str, MAX, fp1);
+                if (!feof(fp1)) {
+                        fputs(str, fp3);
+                }
         }
 
         /* close all opened files */
@@ -64,11 +80,10 @@
         fclose(fp2);
         fclose(fp3);
 
-        /* remove the input files */
-        remove(fname1);
-        remove(fname2);
-        /* rename the temporary file to first file name */
-        rename(temp, fname1);
-
+        /* remove the input file */
+        remove(file1);
         return 0;
   }
+
+
+  
